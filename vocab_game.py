0a11437858e3be55255394fd37823ba4ca57a147
404-35-sirikorn <56779@yupparaj.ac.th>
2026-09-02 20:07:01 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -95,12 +95,19 @@ ans2 = st.text_input(
     "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
     value=st.session_state.ans2_val,
 )
-
+ans3 = st.text_input(
+    "ข้อ 3: I love to eat`man_ _`. 🥭",
+    value=st.session_state.ans3_val,
+)
+ans4 = st.text_input(
+    "ข้อ 4: I love to eat `orange`. 🍊",
+    value=st.session_state.ans4_val,
+)
 # อัปเดตค่าล่าสุดเข้าตัวแปร
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
-
-# 📝 [พื้นที่สำหรับนักเรียน]: เพิ่มข้อ 3, 4 ตรงนี้
+st.session_state.ans3_val = ans3
+st.session_state.ans4_val = ans4
 
 
 # # 4. ปุ่มส่งคำตอบ
